1e37928c121637deee6833ec8be677a1b98484ad
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-01-24 17:00:45 +0200

    update

--- src/components/FeedbackOptions/FeedbackOptions.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.jsx
@@ -16,6 +16,10 @@ const FeedbackOptions = ({ options, onLeaveFeedback }) => {
 
 export default FeedbackOptions;
 
+FeedbackOptions.defaultProps = {
+  options:[],
+}
+
 FeedbackOptions.propTypes ={
   options:PropTypes.arrayOf(PropTypes.string.isRequired),
   onLeaveFeedback:PropTypes.func.isRequired
