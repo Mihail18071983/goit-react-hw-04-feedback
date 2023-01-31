8135cc33ab4b6614a4257bb476254136c3591306
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-01-31 20:10:49 +0200

    update

--- src/App.jsx
+++ src/App.jsx
@@ -1,76 +1,79 @@
-import { Component } from 'react';
+import { useState } from 'react';
 import Statistics from 'components/Statistics/Statistics';
 import FeedbackOptions from 'components/FeedbackOptions/FeedbackOptions';
 import Section from 'components/Section/Section';
 import Notification from 'components/Notification/Notification';
 import styled from 'styled-components';
 
-
 const AppWrapperStyled = styled.div`
   margin-left: 30px;
 `;
 
-export class App extends Component {
-
-  state = {
+const App = () => {
+  const [feedback, setFeedback] = useState({
     good: 0,
     neutral: 0,
     bad: 0,
+  });
+
+  const statePropNames = Object.keys(feedback);
+
+  const onLeaveFeedback = feedback => {
+    setFeedback(prevState => {
+      const value=prevState[feedback]
+      return {
+        ...prevState,
+        [feedback]: value+1,
+      };
+    });
   };
 
-  statePropNames = Object.keys(this.state);
-
-  onLeaveFeedback = feedback => {
-    this.setState(prevState => ({
-      [feedback]: prevState[feedback] + 1,
-    }));
-  };
-
-  countTotalFeedback () {
+  const countTotalFeedback = () => {
     let total = 0;
-    for (const statePropName of this.statePropNames) {
-      total += this.state[statePropName];
+    for (const statePropName of statePropNames) {
+      total += feedback[statePropName];
     }
     return total;
-  };
+  }
 
-  countPositiveFeedbackPercentage() {
-    const total = this.countTotalFeedback();
+  const countPositiveFeedbackPercentage=() =>{
+    const total = countTotalFeedback();
     if (!total) {
       return 0;
     }
     const positiveFidback = Math.round(
-      (this.state.good * 100) / total
+      (feedback.good * 100) / total
     );
     return positiveFidback;
   };
 
-  render() {
-    return (
+  return (
       <AppWrapperStyled>
         <Section title="Please leave the feedback">
           <FeedbackOptions
-            options={this.statePropNames}
-            onLeaveFeedback={this.onLeaveFeedback}
+            options={statePropNames}
+            onLeaveFeedback={onLeaveFeedback}
           />
         </Section>
 
-        {this.countTotalFeedback() !== 0 && (
+        {countTotalFeedback() !== 0 && (
           <Section title="Statistics">
             <Statistics
-              good={this.state.good}
-              neutral={this.state.neutral}
-              bad={this.state.bad}
-              total={this.countTotalFeedback()}
-              positivePercentage={this.countPositiveFeedbackPercentage()}
+              good={feedback.good}
+              neutral={feedback.neutral}
+              bad={feedback.bad}
+              total={countTotalFeedback()}
+              positivePercentage={countPositiveFeedbackPercentage()}
             />
           </Section>
         )}
 
-        {this.countTotalFeedback() === 0 && (
+        {countTotalFeedback() === 0 && (
           <Notification message="There is no feedback" />
         )}
       </AppWrapperStyled>
     );
-  }
-}
+};
+
+export default App;
+
