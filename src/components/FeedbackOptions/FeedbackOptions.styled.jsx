98d983218446833810b299a734b78da7d75701a3
Mihailo <106537730+Mihail18071983@users.noreply.github.com>
2023-02-05 13:30:31 +0200

    update

--- src/components/FeedbackOptions/FeedbackOptions.styled.jsx
+++ src/components/FeedbackOptions/FeedbackOptions.styled.jsx
@@ -1,25 +1,28 @@
 import styled from 'styled-components';
 
-
 export const StyledBtn = styled.button`
-background-color:#fff;
-border: 1px solid teal;
-border-radius:5px;
-color: ${props=>{
-  switch(props.children){
-  case 'good':return "#00ff00";
-  case 'neutral':return "#0000ff";
-  case 'bad': return "#ff0000";
-  default: return "#000"
-}}};
+  background-color: #fff;
+  border: 1px solid teal;
+  border-radius: 5px;
+  color: ${props => {
+    switch (props.children) {
+      case 'good':
+        return '#00ff00';
+      case 'neutral':
+        return '#0000ff';
+      case 'bad':
+        return '#ff0000';
+      default:
+        return '#000';
+    }
+  }};
+  transition: background-color 0.5s cubic-bezier(0.05, 0.97, 1, 0.08),
+    color 0.5s cubic-bezier(0.05, 0.97, 1, 0.08);
 
-&:hover {
-background-color:blue;
-color: black;
-}
+  &:hover {
+    background-color: blue;
+    color: black;
+  }
 `;
 
-export default StyledBtn
-
-
-
+export default StyledBtn;
